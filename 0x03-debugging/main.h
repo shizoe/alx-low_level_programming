ada9aa2bc4d4a33daa1652a60a8d7827183db248
shizoe <71692698+shizoe@users.noreply.github.com>
2022-04-12 18:10:43 +0200

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
 
-#ifndef HOLBERTON_H
-#define HOLBERTON_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 
@@ -9,4 +9,4 @@ int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* HOLBERTON_H */
+#endif /* MAIN_H */
