38e55c27e7982b84f7317c1ceb17dd84a240c600
shizoe <71692698+shizoe@users.noreply.github.com>
2022-05-24 21:10:48 +0200

    Advanced singly linked lists

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,14 +10,15 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t num = 0;
 	long int diff;
-
 	while (head)
 	{
 		diff = head - head->next;
 		num++;
 		printf("[%p] %d\n", (void *)head, head->n);
 		if (diff > 0)
+		{
 			head = head->next;
+		}
 		else
 		{
 			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
